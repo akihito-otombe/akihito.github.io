c10cf646be8e8d6b2758b204f0e5ebefcba591e3
akihito-otombe <akihito.otombe@bee-arc.com>
2025-11-05 20:00:42 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// GitHub Pages で /<リポジトリ名>/ 配下になるため base を必ず設定
 export default defineConfig({
   plugins: [react()],
-  base: '/tsumugi/'   // ← ← ← ここが最重要
+  base: '/tsumugi/',
 })
