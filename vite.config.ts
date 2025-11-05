04c54f41a449d3cff934e303f6b321c815900173
akihito-otombe <akihito.otombe@bee-arc.com>
2025-11-05 19:30:12 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// GitHub Pages の公開パス（ユーザー名/リポジトリ名が tsumugi の想定）
 export default defineConfig({
   plugins: [react()],
-  base: '/tsumugi/', // ← リポジトリ名に合わせる
+  base: '/tsumugi/'
 })
